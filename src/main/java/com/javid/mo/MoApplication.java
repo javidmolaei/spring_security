b9e8b08f7cde3d699274cac553b85bdc15d603d4
javidmolaei <javidmolaei@gmail.com>
2023-05-27 20:52:29 +0430

    first commit

---
+++ src/main/java/com/javid/mo/MoApplication.java
@@ -0,0 +1,13 @@
+package com.javid.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoApplication.class, args);
+	}
+
+}
